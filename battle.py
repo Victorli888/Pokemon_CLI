f8e36875a9f236bc68339d486a7dc76d9743e5f5
Victor <victorli7462@gmail.com>
2020-01-20 15:23:07 -0800

    re commit

---
+++ battle.py
@@ -0,0 +1,16 @@
+class wild_poke(object):
+
+    def __init__(self, pokemon_name):
+        self.pokemon_name = pokemon_name
+        print(f"A wild {pokemon_name} appeared...")
+
+pidgey = wild_poke("Pidgey")
+rattata = wild_poke("Rattata")
+
+# intro for a battle works, I think I should make a switch case or a dictioanry
+# that pulls these names to make it seem like it randomly appears when wandering through a bush
+
+
+
+
+
