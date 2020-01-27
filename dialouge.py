3f9afc8d979395fbde954581323629ada0bf0d11
Victor Li <victorli@victors-mbp.attlocal.net>
2020-01-27 00:18:27 -0800

    Added main story.py and dialouge

---
+++ dialouge.py
@@ -0,0 +1,6 @@
+class Profesor():
+     def __init__(self, name):
+         self.name = name
+
+     def diag_1:
+         print( f"Ah... hello {name}, my name is Professor Oak")
---
+++ story.py
@@ -0,0 +1,16 @@
+print("Welcome to the world of Pokemon! Where we enslave animals big and large to fight and perform for our pleasure")
+
+def player_config():
+    global gender
+    print("Please tell me are you a Boy or a Girl?")
+    gender = input("> ")
+
+    if gender == "Boy" or gender == "Girl":
+            print(gender)
+
+    else:
+        print("That's not what I asked for...")
+        player_config()
+
+player_config()
+print(gender)
