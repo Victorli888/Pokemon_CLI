0afab86f39850d8a5019eac33a042c7e7811e236
Victorli888 <victorli7462@gmail.com>
2020-06-21 13:37:39 -0700

    fixed indentation issues with dialouge page

--- dialouge.py
+++ dialouge.py
@@ -2,6 +2,8 @@
 All in character dialouges will be stored in this script.py file, All characters are organized in Alphabet order.
 """
 import Actions
+
+
 def proffesor_diag_1(player_name):
     print(f"Ah... hello {player_name}, my name is Professor Oak")
     Actions.tap()
@@ -9,6 +11,8 @@ def proffesor_diag_1(player_name):
     Actions.tap()
     print(f"Are you ready to start your adventure?")
     Actions.tap()
+
+
 def proffesor_diag_2():
     print("On the table are 3 Pokemon, Please choose wisely... ")
     Actions.tap()
@@ -18,32 +22,36 @@ def proffesor_diag_2():
     Actions.tap()
     print("3rd we have Cyndaquill a Fire type")
     Actions.tap()
+
+
 def proffesor_diag_3():
     print("Great Pick please take care of them and cherish your Pokemon the rest of your life")
     Actions.tap()
     print("Before I forget go see my assistant for your PokeDex")
     Actions.tap()
 
+
 def assistant_diag_1():
-        print("Oh Hi there, I'm Proffesor Oak's Lab Assitant. I'm pretty busy at the moment. why don't you speak with\n"
-              "the Professor first.")
-        Actions.tap()
+    print("Oh Hi there, I'm Proffesor Oak's Lab Assitant. I'm pretty busy at the moment. why don't you speak with\n"
+          "the Professor first.")
+    Actions.tap()
+
 
 def assistant_diag_2():
-        print("Oh hi there, Oak asked me to give you something?")
-        Actions.tap()
-        print("... OH RIGHT, here you go.... A Pokedex, you can use it to log and learn more about Pokemon")
-        Actions.tap()
-
+    print("Oh hi there, Oak asked me to give you something?")
+    Actions.tap()
+    print("... OH RIGHT, here you go.... A Pokedex, you can use it to log and learn more about Pokemon")
+    Actions.tap()
 
 
 def mom_diag_1(player_name):
     print(f"{player_name}, You're finally up! I was worried you were going to sleep the whole day away!")
     Actions.tap()
     print("I wanted to let you know that, Professor Oak wants to talk to you! But you should do that after"
-              "\na bit of breakfast.")
+          "\na bit of breakfast.")
     Actions.tap()
+
+
 def mom_diag_2():
     print("Why hello dear! I'm a little busy with house work, so why don't you go enjoy how nice it is outside. ")
     Actions.tap()
-
