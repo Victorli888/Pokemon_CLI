2e79aca870a785d7feb492184f50f815c5cb090d
Victorli888 <victorli7462@gmail.com>
2020-06-21 13:55:10 -0700

    added skelton code for moves, and speed stat

--- poke_characters.py
+++ poke_characters.py
@@ -1,3 +1,5 @@
+import Actions
+
 """
 mv1 = moveset 1 (attack move, power up move , etc)
 pokemon_l5 = pokemon name and its level
@@ -7,17 +9,55 @@ effect in game when called on this will be under
 
 
 class Pokemon:
-    def __init__(self, name, hp, attack, defence, mv1, mv2, mv3, mv4):
+    def __init__(self, name, hp, attack, defence, speed, mv1, mv2, mv3, mv4):
         self.name = name
         self.hp = hp
         self.attack = attack
         self.defence = defence
+        self.speed = speed
         self.mv1 = mv1
         self.mv2 = mv2
         self.mv3 = mv3
         self.mv4 = mv4
+    """
+    Chance to Hit: Critical Chance: 20% 
+    """
+    def tackle(self):
+        print(f"{self.name} Used Tackle...")
+        Actions.tap()
+        # Check for Hit/Miss
+        # Check for Critical
+        # Check stats and reference damage accordingly
+
+    def scratch(self):
+        print(f"{self.name} Used Scratch...")
+        Actions.tap()
+        # Check for Hit/Miss
+        # Check for Critical
+        # Check stats and reference damage accordingly
+
+    def smokescreen(self):
+        print(f"{self.name} used Smokescreen...")
+        Actions.tap()
+        # Check for Hit or Miss
+        # Apply Status effect
+
+    def leer(self):
+        print(f"{self.name} used Leer...")
+        Actions.tap()
+        # Check for Hit or Miss
+        # Apply Status Effect
+
+    def growl(self):
+        print(f"{self.name} used Growl...")
+        Actions.tap()
+        # Check for Hit or Miss
+        # Apple Status Effect
 
 
-cyndiquil_l5 = Pokemon("Cyniquil", 39, 52, 43, "tackle", "smokescreen", " ", " ")
-chikorita_l5 = Pokemon("Chikorita", 45, 49, 65, "tackle", "growl", " ", " ")
-totodile_l5 = Pokemon("Totodile", 50, 65, 64, "scratch", "leer", " ", " ")
+
+cyndiquil_l5 = Pokemon("Cyndiquil", 39, 52, 43, 0, "tackle", "smokescreen", " ", " ")
+chikorita_l5 = Pokemon("Chikorita", 45, 49, 65, 0, "tackle", "growl", " ", " ")
+totodile_l5 = Pokemon("Totodile", 50, 65, 64, 0, "scratch", "leer", " ", " ")
+
+totodile_l5.tackle()
